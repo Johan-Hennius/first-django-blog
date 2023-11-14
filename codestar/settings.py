07c927eacce36380700d9637d98bfe2f64e0a911
Johan Hennius <johan.bh19@gmail.com>
2023-11-14 11:17:58 +0000

    Add heroku url to allowed_hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-johanhenniu-firstdjango-5xwyie9gfea.ws-eu106.gitpod.io', 'jbh19-codestar-blog-4effcaa46d09.herokuapp.com']
 
