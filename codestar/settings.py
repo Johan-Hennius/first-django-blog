6d714163222700159e44db7f0611535e15a549f1
Johan Hennius <johan.bh19@gmail.com>
2023-11-14 11:07:38 +0000

    Add host to CSRF and create blog table

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-johanhenniu-firstdjango-5xwyie9gfea.ws-eu106.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-johanhenniu-firstdjango-5xwyie9gfea.ws-eu106.gitpod.io']
 
 
 # Application definition
@@ -88,6 +88,13 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.gitpod.io",
+    "https://8000-johanhenniu-firstdjango-5xwyie9gfea.ws-eu106.gitpod.io"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
