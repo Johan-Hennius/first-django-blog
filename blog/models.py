7c8acc2dbd883359ef8a6de5931b4dd6b16d9ae3
Johan Hennius <johan.bh19@gmail.com>
2023-11-14 15:06:57 +0000

    Finished migration recovery exercises

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     excerpt = models.TextField(blank=True)
@@ -14,6 +15,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     update_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -21,3 +28,9 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.body} by {self.author}"
