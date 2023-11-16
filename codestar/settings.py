0a7f632ef5196117803e4deb6a3c545e7160dc4f
Johan Hennius <johan.bh19@gmail.com>
2023-11-16 09:59:12 +0000

    Add saved files

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['8000-johanhenniu-firstdjango-5xwyie9gfea.ws-eu106.gitpod.io', 'jbh19-codestar-blog-4effcaa46d09.herokuapp.com', ".gitpod.io"]
 
