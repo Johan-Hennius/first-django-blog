d33a177e31f7e45b5e868108095637610c03f8cb
Johan Hennius <johan.bh19@gmail.com>
2023-11-17 13:27:36 +0000

    Post request handling complete

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -7,6 +8,16 @@ from .forms import CollaborateForm
 def about_me(request):
 
     about = About.objects.all().order_by('-updated_on').first()
+
+    if request.method == "POST":
+        collaborate_request = CollaborateForm(data=request.POST)
+        if collaborate_request.is_valid():
+            collaborate_request.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days."
+            )
+
     collaborate_request = CollaborateForm()
 
     return render (
