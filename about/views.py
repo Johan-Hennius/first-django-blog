6e21b1902b62313be992f06a6b0fb7aa1d2a5e01
Johan Hennius <johan.bh19@gmail.com>
2023-11-17 11:50:38 +0000

    Add contact form to about page

--- about/views.py
+++ about/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_me(request):
 
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_request = CollaborateForm()
 
     return render (
         request,
         "about/about.html",
         {
-            "about": about
+            "about": about,
+            "collaborate_request": collaborate_request,
         }
     )
--- blog/views.py
+++ blog/views.py
@@ -50,6 +50,9 @@ def post_detail(request, slug):
     )
 
 
+
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
